35a9554c85baf31008ab1adee4e8d45797483dbb
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-01-14 08:02:37 +0200

    finishing intro card and its responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroCardComponent } from './components/intro-card/intro-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

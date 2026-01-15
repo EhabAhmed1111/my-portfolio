d699ea71fb79b9a92aa5ba086af0a7daabf850fc
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-01-15 23:09:47 +0200

    finish about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { IntroCardComponent } from './components/intro-card/intro-card.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IntroCardComponent, HeaderComponent],
+  imports: [RouterOutlet, IntroCardComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
 export const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: 'about' }
+  { path: '', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: '**', redirectTo: '' }
 ];

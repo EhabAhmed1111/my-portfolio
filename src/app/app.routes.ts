b10fcf2d8085d91d01e78d4959fed3e99d222775
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-01-15 20:56:46 +0200

    create about me section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
-  
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: 'about' }
 ];
--- src/app/components/intro-card/intro-card.component.ts
+++ src/app/components/intro-card/intro-card.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class IntroCardComponent {
 
+  isContactsOpen: boolean = false;
+
 }

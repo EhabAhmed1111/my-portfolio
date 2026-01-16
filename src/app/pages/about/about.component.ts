1003a54e5548ef7340f49b4abffda8f966b98409
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-01-16 05:23:04 +0200

    finish project&resume sections

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
 
+  onSubmit() {
+    console.log(this.name, this.email, this.message);
+  }
 }
